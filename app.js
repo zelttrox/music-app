dbac5f62f683c0e3b45474adc35252d999c30839
enzo <enzo.granana@ynov.com>
2025-04-04 18:04:50 +0200

    Setup html render

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const ejs = require('ejs');
 
 function StartServer() {
 
-server.get('/', (request, response) => {
-    response.send('Hello World!');
+// Initialize Express framework
+const server = express();
+
+// Setup server route
+server.get('/', function(request, response) {
+    response.render('index')
   });
 
-server.listen(3030, () => {
+// Set render engine
+server.set('view engine', 'ejs')
+
+// Listen to provided port
+server.listen(3030, function() {
     console.log('Server started on port 3000');
   });
-
 }
 
-StartServer()
+StartServer();
