d7ee9b8b93a44be75fcfbd9f6030e453c9d98ab2
enzo <enzo.granana@ynov.com>
2025-04-04 18:09:58 +0200

    Changes server init

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const ejs = require('ejs');
 
-function StartServer() {
-
-// Initialize Express framework
 const server = express();
 
-// Setup server route
-server.get('/', function(request, response) {
-    response.render('index')
-  });
-
-// Set render engine
 server.set('view engine', 'ejs')
 
-// Listen to provided port
-server.listen(3030, function() {
-    console.log('Server started on port 3000');
-  });
-}
+server.get('/', function (request, response) {
+    response.render('index')
+});
 
-StartServer();
+server.listen(3030, function () {
+    console.log('Server started on port 3030');
+});
