bf1ae1d3e4aed9323f4a138ae1044782da4bee4b
enzo <enzo.granana@ynov.com>
2025-04-05 22:51:27 +0200

    Add ID handler for songs routes

--- routes/songs.js
+++ routes/songs.js
@@ -1,8 +1,38 @@
 const express = require("express")
 const router = express.Router()
 
+class Song {
+    constructor(name, artist, path) {
+        this.name = name
+        this.artist = artist
+        this.path = path
+        this.id = (`${this.name}-${this.artist}`).toLowerCase()
+        console.log("Successfully added song with ID: ", this.id)
+    }
+}
+
+var songs = [
+    new Song("Nostalgia", "Suki_Waterhouse", "./music/Nostalgia.mp3")
+]
+
 router.get("/", function (request, response) {
     response.send("Songs")
+})
+
+router.route("/:id").get( function(request, response) {
+    console.log(request.song)
+    response.send(`Song: ${request.song.name} by ${request.song.artist}`)
+})
+
+router.param("id", function (request, response, next, id) {
+    var target = songs.find(function(song) {return song.id == id})
+    if (target) {
+        request.song = target
+        next()
+    }
+    else {
+        response.send("Could not find song for ID")
+    }
 })
 
 module.exports = router
