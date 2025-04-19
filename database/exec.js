6c226e988036d9af5b6814262ee4b92be32d7df9
enzo <enzo.granana@ynov.com>
2025-04-19 19:28:54 +0200

    Revert "Add promises to database functions"
    This reverts commit cbcde46e04e9f7a1ad9271b7227c2344936c9c99.

--- database/exec.js
+++ database/exec.js
@@ -1,9 +1,7 @@
 // Imports
 const mysql = require("mysql2");
 const fs = require("fs")
-const path = require("path");
-const { resolveAny } = require("dns/promises");
-const { rejects } = require("assert");
+const path = require("path")
 
 // Setup connection with root user
 const database = mysql.createConnection({
@@ -16,75 +14,54 @@ const database = mysql.createConnection({
 
 // Attempt connection to the database
 function Connect() {
-return new Promise((resolve, reject) => {
-    console.log("[DB] Attempting to connect to the database..")
     database.connect((err) => {
+        console.log("[DB] Attempting to connect to the database..")
         if (err != null) {
             console.error("[DB] Error connecting to the database: ", err)
-            reject(err)
-        } 
+        }
         else {
             console.log("[DB] Successfully connected to the database!")
-            resolve()
         }
     })
-})}
+}
 
-// Disconnect the database
 function Disconnect() {
-return new Promise((resolve, reject) => {
-   database.end((err) => {
-   if (err != null) {
-      console.log("[DB] Successfully disconnected database!")
-      resolve()
-   }
-   else reject()
-   })
-})}
+    database.end()
+    console.log("[DB] Successfully disconnected database!")
+}
 
-// Initialize database using SQL instructions
 function Init() {
-return new Promise((resolve, reject) => {
-var file_path = path.join(__dirname, "init.sql")
+    var file_path = path.join(__dirname, "init.sql")
     var sql_script = fs.readFileSync(file_path, "utf-8")
     database.query(sql_script, function (err, output) {
         if (err != null) {
             console.log("[DB] Error while initializing database: ", err)
-            reject()
+            return
         }
         else {
             console.log("[DB] Successfully initialized the database!")
-            resolve()
         }
     })
-})}
+}
 
-// Attempt to query database
 function Query(query, ...args) {
-return new Promise((resolve, reject) => {
-   database.query(query, args, (err) => {
-      if (err != null) {
-         console.log("[DB] Error while executing database with query: ", query)
-         console.log("[DB] ", err)
-         reject()
-      }
-      else resolve()
-   })
-})}
-
-// Attempt to get data from the database
-function GetQuery(query) {
-return new Promise((resolve, reject) => {
-   database.query(query, (err, output) => {
-      if (err != null) {
-         console.log("[DB] Error while get querying database with query: ", query)
-         console.log(["DB: ", err])
-         reject()
-      }
-      else resolve()
-      return output
+    database.query(query, args, (err) => {
+        if (err != null) {
+            console.log("[DB] Error while executing database with query: ", query)
+            console.log("[DB] ", err)
+        }
     })
-})}
+}
+
+function GetQuery(query) {
+    database.query(query, (err, output) => {
+        if (err != null) {
+            console.log("[DB] Error while get querying database with query: ", query)
+            console.log(["DB: ", err])
+        }
+        return output
+    })
+}
 
 // Exports
 module.exports = {
