aaeab93e388c06de8fd81c9f5a0a4e77a871667f
enzo <enzo.granana@ynov.com>
2025-05-03 11:21:52 +0200

    Install bcrypt + add hash password func

--- src/security.js
+++ src/security.js
@@ -1,3 +1,6 @@
+// Import modules
+const bcrypt = require("bcrypt")
+
 // Define common SQL instructions and identifiers
 common = ["105", "105 OR 1=1", "\" or \"\"=\""]
 identifiers = ["*", "'", "$", "=", ",", ";", "\""]
@@ -5,14 +8,27 @@ identifiers = ["*", "'", "$", "=", ",", ";", "\""]
 // Check if a given input is an injected SQL instruction using regex
 function IsSQL(input) {
     input = input.toString()
-    common.forEach(expr => {if (input.includes(expr)) return true})
-    identifiers.forEach(term => {if (input.includes(term)) return true})
-    console.log("[Security] No SQL injection detected")
-    return false
+    var is_sql = false
+    common.forEach(expr => {if (input.includes(expr)) is_sql = true})
+    identifiers.forEach(term => {if (input.includes(term)) is_sql = true})
+    console.log("[Security] SQL Injection:", is_sql)
+    return is_sql
 }
+
+// Return a hashed given password using bcrypt
+async function HashPasswd(password) {
+    const hashedPassword = await bcrypt.hash(password, salt)
+    return hashedPassword
+}
+
+// Generate password salt using bcrypt
+var salt
+async function GenerateSalt() {salt = await bcrypt.genSalt()}
+GenerateSalt()
 
 
 // Exports
 module.exports = {
-    IsSQL
+    IsSQL,
+    HashPasswd
 }
